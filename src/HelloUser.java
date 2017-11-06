1c4f2e721440165af4fe36d2ecb409c4da8c90a5
Felix Feik <felix@feik-web.de>
2017-11-06 10:01:18 +0100

    HelloUser is added

---
+++ src/HelloUser.java
@@ -0,0 +1,12 @@
+public class HelloUser {
+	
+	public String userName;
+	
+	public HelloUser(String userName){
+		this.userName = userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello " + userName + "!");
+	}
+}
